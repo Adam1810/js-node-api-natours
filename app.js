cd4e2647a20d81cc571daa14e2c1a0b94727ab53
Adam1810 <dareal1810@googlemail.com>
2021-02-18 22:19:11 +0100

    updated

--- app.js
+++ app.js
@@ -19,7 +19,43 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/vi/tours', (req, res) => {});
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params)
+  const id = req.params.id * 1;
+  if(id > tours.length){
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid tour id"
+    })
+  }
+
+  const tour = tours.find(el => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId}, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({status: 'sucess', data: { tour: newTour} })
+  })
+
+app.patch('/api/v1/tours', (req, res) => {
+  res.status(200).json({status: "success", data: {tour: 'updated tour her'}})
+})
+});
+
+
 
 const port = 3000;
 app.listen(port, () => {
