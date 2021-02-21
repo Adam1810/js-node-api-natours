c3ee1796e1dfa2f686b5ff7ec271c630ade7231e
Adam1810 <dareal1810@googlemail.com>
2021-02-21 15:48:42 +0100

    update

---
+++ models/tourModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const tourchSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourchSchema);
+
+module.exports = Tour;
